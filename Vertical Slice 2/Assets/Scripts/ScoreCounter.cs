096ccd358bd43a7f7c747baceeaaba839e69fe5f
Falkra <justinudo959@gmail.com>
2019-01-14 11:01:15 +0100

    Added: Score Script Added: Score_UI

---
+++ Vertical Slice 2/Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour {
+
+    private Text scoreText; //Creates Text
+
+    private int score; //Creates the score
+
+    public int Score //Get % Sets score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    void Start () {
+
+        scoreText = GetComponent<Text>();
+        scoreText.text = "Score: " + 0;
+        score = 0;
+	}
+
+
+}
