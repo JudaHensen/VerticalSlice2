cfc95aecf63a88a7f7493452c78c7e51fd9ac707
JudaHensen <24311@ma-web.nl>
2019-01-17 10:13:43 +0100

    Added LevelSystem script > Not Done yet

---
+++ Vertical Slice 2/Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour {
+
+    [SerializeField]
+    private int currentLevel, amountOfWaves;
+    [SerializeField]
+    private float enemy
+
+}
