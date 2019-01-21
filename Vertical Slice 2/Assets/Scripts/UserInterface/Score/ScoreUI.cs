e71fa2fc5c15123c716e8ec1a83c62e94615322b
JudaHensen <24311@ma-web.nl>
2019-01-21 11:35:31 +0100

    Merged files
    - Added ScoreSystem
    - Added Healthbar
    
    - Removed InputHandler > Mike works further on this.
    - Removed PlayerMovement > Mike works further on this.

---
+++ Vertical Slice 2/Assets/Scripts/UserInterface/Score/ScoreUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUI : MonoBehaviour {
+
+    [SerializeField]
+    private string message;
+
+    private Text text;
+    private ScoreSystem scoreSystem;
+
+    void Awake () {
+        text = GetComponent<Text>();
+        scoreSystem = GameObject.FindGameObjectWithTag("ScoreSystem").GetComponent<ScoreSystem>();
+
+        scoreSystem.ScoreUpdate += UpdateDisplay;
+    }
+
+    private void UpdateDisplay(int value)
+    {
+        text.text = message + value.ToString();
+    }
+
+}
