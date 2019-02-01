e22edee64b0dff3b280b4a194b94048b33702811
JudaHensen <24311@ma-web.nl>
2019-02-01 15:03:51 +0100

    Fixed lightning system

---
+++ Vertical Slice 2/Assets/Scripts/Player/LightFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour {
+
+    public Transform target;
+
+	void Update () {
+
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, 0.390625f);
+
+	}
+}
